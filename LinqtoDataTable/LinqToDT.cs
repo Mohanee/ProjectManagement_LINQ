76b4c6e025d9d832578a2fa5b9bc0a4f22eca62b
Mohanee <mohanee99@gmail.com>
2020-11-07 11:00:12 +0530

    [mohanee]initial commit

---
+++ LinqtoDataTable/LinqToDT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace LinqtoDataTable
+{
+    class LinqToDT
+    {
+        public void AddToDataTableDemo()
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID");
+            table.Columns.Add("ProductName");
+
+            table.Rows.Add("1", "Chai");
+            table.Rows.Add("2", "Queso Carbels");
+            table.Rows.Add("3", "Tofu");
+            DisplayProductFromTable(table);
+        }
+        public void DisplayProductFromTable(DataTable table)
+        {
+            var productNames = from products in table.AsEnumerable() select products.Field<string>("ProductName");
+            Console.WriteLine("Product Name: ");
+            foreach (string productName in productNames)
+            {
+                Console.WriteLine(productName);
+            }
+        }
+    }
+}
---
+++ LinqtoDataTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinqtoDataTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Data Table demo");
+            LinqToDT linqToDataTable = new LinqToDT();
+            linqToDataTable.AddToDataTableDemo();
+            Console.ReadKey();
+
+
+        }
+    }
+}
