ac8dbded350eb02bed03d21faaf60f8c44312d82
Mohanee <mohanee99@gmail.com>
2020-11-10 23:36:19 +0530

    [mohanee]Add UseCases upto UC4 done in class

---
+++ LinqtoDataTable/ManagementClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace LinqtoDataTable
+{
+    class ManagementClass
+    {
+        //UC1 Create DatatTable
+        /// <summary>
+        /// Creating the datatTable
+        /// </summary>
+        public readonly DataTable dataTable = new DataTable();
+
+        //UC2 List top 3 records with high ratings
+        public void Top3Records(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from ProductReviews in listProductReview
+                                orderby ProductReviews.Rating descending
+                                select ProductReviews).Take(3);
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine(list.ProductID);
+            }
+        }
+
+        //UC3
+        /// <summary>
+        /// Method to retrieve records with rating >3 and project id = 1/4/9
+        /// </summary>
+        /// <param name="listProductReview"></param>
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = from productReviews in listProductReview
+                             where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                             && productReviews.Rating > 3
+                             select productReviews;
+            foreach (var list in recordData)
+            {
+                Console.WriteLine(list.ProductID);
+            }
+        }
+
+        //UC4
+        /// <summary>
+        /// Method to List count of review present for each record
+        /// </summary>
+        /// <param name="listProductReview"></param>
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+            var recordData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            foreach (var list in recordData)
+            {
+                Console.WriteLine(list.ProductID + "-----------" + list.Count);
+            }
+        }
+    }
+}
--- LinqtoDataTable/Program.cs
+++ LinqtoDataTable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LinqtoDataTable
 {
@@ -6,13 +7,36 @@ namespace LinqtoDataTable
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome to Data Table demo");
-            LinqToDT linqToDataTable = new LinqToDT();
-            linqToDataTable.AddToDataTableDemo();
-            Console.ReadKey();
+            Console.WriteLine("==========================");
+            //LinqToDataTable linqToDataTableObj = new LinqToDataTable();
+            //linqToDataTableObj.AddDataTableDemo();
 
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview(){ProductID=1,UserID=1,Rating=2,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=4,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=3,UserID=1,Rating=5,Review="Good",IsLike=true},
+                new ProductReview(){ProductID=4,UserID=1,Rating=6,Review="Good",IsLike=false},
+                new ProductReview(){ProductID=5,UserID=1,Rating=2,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=6,UserID=1,Rating=1,Review="bas",IsLike=true},
+                new ProductReview(){ProductID=8,UserID=1,Rating=1,Review="Good",IsLike=false},
+                new ProductReview(){ProductID=8,UserID=1,Rating=9,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=10,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=10,UserID=1,Rating=8,Review="nice",IsLike=true},
+                new ProductReview(){ProductID=11,UserID=1,Rating=3,Review="nice",IsLike=true}
+            };
 
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine($"{list.ProductID},{list.UserID},{list.Rating},{list.Review},{list.IsLike}");
+            }
+
+            ManagementClass management = new ManagementClass();
+            management.TopRecords(productReviewList);
+
+            management.SelectedRecords(productReviewList);
+            management.RetrieveCountOfRecords(productReviewList);
         }
     }
 }
